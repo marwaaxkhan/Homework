e99765e178c753cae4f2d700eb3a7800624873c5
Marwa Khan <marwakhan@Marwas-MBP.home>
2021-07-23 16:51:00 -0400

    07232021 4:50pm

---
+++ src/hw1Q2JavaBasics01/HelloWorld.java
@@ -0,0 +1,12 @@
+package hw1Q2JavaBasics01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		System.out.println("My name is Marwa Khan.");
+	}
+
+}
---
+++ src/hw1Q2javaBasics/Hello.java
@@ -0,0 +1,11 @@
+package hw1Q2javaBasics;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
